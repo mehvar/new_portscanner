24e5ab75e314a9bb1b2be1d3a8c26b7ede681322
mehvar <justmehvar@gmail.com>
2017-10-25 05:11:13 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,2 @@
+<body onload="setTimeout(Mehvar, 0);" oncontextmenu="return false" onkeydown="return false" bgcolor="black">
+<script>function Mehvar() {if(screen.width<=480) top.window.location.href="mob.php"; else window.location="web.php";}</script>
